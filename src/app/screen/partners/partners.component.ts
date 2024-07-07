10439456f71e269c65c536573272f9e3e9a5bc70
Your NameAhmed <you@example.commedomansour872@gmail.com>
2024-07-07 12:40:17 +0300

    save changes

--- src/app/screen/partners/partners.component.ts
+++ src/app/screen/partners/partners.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./partners.component.scss']
 })
 export class PartnersComponent {
-
+  images: any[] = [
+    {img:'./assets/images/oem/oem1.png'},
+    {img:'./assets/images/oem/oem2.png'},
+    {img:'./assets/images/oem/oem3.png'},
+    {img:'./assets/images/oem/oem4.png'},
+    {img:'./assets/images/oem/oem5.png'},
+    {img:'./assets/images/oem/oem6.png'},
+    {img:'./assets/images/oem/oem7.png'},
+    {img:'./assets/images/oem/oem8.png'},
+    {img:'./assets/images/oem/oem9.png'},
+  ]
 }
